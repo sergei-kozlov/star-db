a465eb964117e88f9e83c4d6e4c446ea087c72a5
Sergei Kozlov <svkozlow@gmail.com>
2019-07-26 16:02:30 +0300

    Project structure

---
+++ src/components/item-list/item-list.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+import './item-list.css';
+
+export default class ItemList extends Component {
+
+    render() {
+        return (
+            <ul className="item-list list-group">
+                <li className="list-groupe-item">
+                    Luke Skywalker
+                </li>
+                <li className="list-groupe-item">
+                    Darth Vader
+                </li>
+                <li className="list-groupe-item">
+                    R2-D2
+                </li>
+            </ul>
+        );
+    }
+}
